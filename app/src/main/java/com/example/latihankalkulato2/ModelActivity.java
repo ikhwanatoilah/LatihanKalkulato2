436da449c653b9c76cb61f843d7b8566f39e0b65
Ikhwan Atoilah <ikhwanatoilah413@gmail.com>
2019-09-01 12:29:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihankalkulato2/ModelActivity.java
@@ -0,0 +1,29 @@
+package com.example.latihankalkulato2;
+
+class ModelActivity {
+    double m_num1, m_num2;
+
+    //constructor
+    public ModelActivity(double num1, double num2){
+        this.m_num1 = num1;
+        this.m_num2 = num2;
+    }
+
+    //setters
+    public void setNum1(double num1){
+        this.m_num1 = num1;
+    }
+
+    public void setNum2(double num2){
+        this.m_num2 = num2;
+    }
+
+    //getters
+    public double getNum1(){
+        return this.m_num1;
+    }
+
+    public double getNum2(){
+        return this.m_num2;
+    }
+}
